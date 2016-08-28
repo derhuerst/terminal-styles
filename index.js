e23f19327b4416d36fd2c300a231a70149aa5c3f
Jannis R <mail@jannisr.de>
2016-08-28 20:35:34 +0200

    implementation :clap:

--- index.js
+++ index.js
@@ -1,3 +1,36 @@
 'use strict'
 
-// todo
+const ansi = require('ansi-styles')
+
+
+
+const end = Object.freeze({})
+
+const render = (parts, ...markers) => {
+
+	const stack = []
+	const escapes = []
+	for (let i = 0; i < markers.length; i++) {
+
+		if (markers[i] === end) {
+			const matching = stack.pop()
+			const outer = stack[stack.length - 1]
+			escapes.push(matching.close + (outer ? outer.open : ''))
+		} else {
+			stack.push(markers[i])
+			escapes.push(markers[i].open)
+		}
+
+	}
+	if (stack.length > 0)
+		throw new Error('start & end markers not balanced')
+
+	let out = ''
+	for (let i = 0; i < parts.length; i++) {
+		out += parts[i]
+		if (i < escapes.length) out += escapes[i]
+	}
+	return out
+}
+
+module.exports = Object.assign(render, ansi, {end})
