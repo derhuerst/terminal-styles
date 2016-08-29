93b23eddb0f511e1d309745dfab38dcd8bf6195e
Jannis R <mail@jannisr.de>
2016-08-29 18:35:39 +0200

    ANSI blacklist & isColor property

--- index.js
+++ index.js
@@ -4,7 +4,28 @@ const ansi = require('ansi-styles')
 
 
 
-const end = Object.freeze({})
+// reset & reverse are problematic because they also affect colors
+const blacklist = ['reset', 'reverse']
+
+// closing tags of colors have a weird behavior, so we need to tag them
+const colors = [
+	'black', 'red', 'green', 'yellow', 'blue', 'magenta', 'cyan', 'white', 'gray',
+	'bgBlack', 'bgRed', 'bgGreen', 'bgYellow', 'bgBlue', 'bgMagenta', 'bgCyan', 'bgWhite', 'bgGray'
+]
+
+const styles = Object.keys(ansi)
+.filter((style) => blacklist.indexOf(style) < 0)
+// todo: consider lodash.omit for this
+.reduce((all, style) => {
+	all[style] = Object.assign({}, ansi[style], {
+		isColor: colors.indexOf(style) >= 0
+	})
+	return all
+}, {})
+
+const end = Object.freeze({end: true})
+
+
 
 const render = (parts, ...markers) => {
 
@@ -33,4 +54,4 @@ const render = (parts, ...markers) => {
 	return out
 }
 
-module.exports = Object.assign(render, ansi, {end})
+module.exports = Object.assign(render, styles, {x: end})
