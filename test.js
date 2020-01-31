f626f53d94323b6ead0c3a7a295e559fef0407f6
Jannis R <mail@jannisr.de>
2020-01-31 23:33:16 +0100

    tape -> assert

--- test.js
+++ test.js
@@ -1,26 +1,22 @@
 #!/usr/bin/env node
 'use strict'
 
-const test = require('tape')
+const {strictEqual} = require('assert')
 
 const styles = require('./index')
 const {red, bold, blue, x} = styles
 
-
-
-test('renders correctly', (t) => {
-	t.plan(1)
-
-	const a = styles `${red}foo ${bold}bar ${blue}baz${x} qux${x} qax${x}`
-	const b = [
-		red.open, 'foo ',
-			bold.open, 'bar ',
-				blue.open, 'baz', blue.close,
-				red.open,
-			' qux', bold.close,
+const actual = styles `${red}foo ${bold}bar ${blue}baz${x} qux${x} qax${x}`
+const expected = [
+	red.open, 'foo ',
+		bold.open, 'bar ',
+			blue.open, 'baz', blue.close,
 			red.open,
-		' qax', red.close
-	].join('')
+		' qux', bold.close,
+		red.open,
+	' qax', red.close
+].join('')
 
-	t.equal(a, b, 'rendered incorrectly')
-})
+strictEqual(actual, expected, 'rendered incorrectly')
+
+console.info('renders correctly!')
